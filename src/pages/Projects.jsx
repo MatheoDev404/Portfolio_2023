72c501dce519319b85c188081dc546146358a23a
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2023-06-05 02:16:58 +0200

    page projets

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,45 @@
+import Card from '../components/Card'
+import { useState, useEffect } from 'react'
+
+function Projects() {
+  const [projects, setProjects] = useState([])
+
+  const getData = () => {
+    fetch('datas/projects.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    })
+      .then(function (response) {
+        return response.json()
+      })
+      .then(function (projects) {
+        setProjects(projects)
+      })
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  return (
+    <div className="Projects">
+      <h1 className="Projects__title">Projets</h1>
+      <section className="Projects__gallery">
+        {projects.map((project) => (
+          <Card
+            key={project.id}
+            id={project.id}
+            title={project.title}
+            picture={project.cover}
+            tags={project.tags}
+            description={project.description}
+          />
+        ))}
+      </section>
+    </div>
+  )
+}
+
+export default Projects
