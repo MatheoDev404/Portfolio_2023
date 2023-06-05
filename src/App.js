9ca7a990a7c3d1e23ad09aec2f156110b8b55945
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2023-06-05 02:15:50 +0200

    Modifications App.js et index.js
    - supression des choses inutiles
    - mise en place du routage sur App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// PAGES
+import Projects from './pages/Projects'
+import Home from './pages/Home'
+
+// COMPONENTS
+import Header from './components/Header'
+
+import './styles/App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Header />
+
+      <Routes>
+        <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/Projets" element={<Projects />}></Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
