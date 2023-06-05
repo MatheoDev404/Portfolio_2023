e811a9ddc60c5236dc6aba5385ed9c46fdfefcd2
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2023-06-05 03:49:31 +0200

    correction import/routages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 // PAGES
 import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Project from './pages/Project'
 import Page404 from './pages/Error'
 
 // COMPONENTS
@@ -16,6 +18,8 @@ function App() {
 
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
+        <Route exact path="/projets" element={<Projects />}></Route>
+        <Route path="/projet/:id" element={<Project />}></Route>
         <Route path="*" element={<Page404 />}></Route>
       </Routes>
     </Router>
