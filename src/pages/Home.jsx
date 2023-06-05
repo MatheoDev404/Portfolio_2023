cc6225c8974f7e22d55300d95df5522d2af40e00
Mathéo Stunault <39770229+MatheoDev404@users.noreply.github.com>
2023-06-05 02:16:49 +0200

    page Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,30 @@
+function Home() {
+  return (
+    <div className="Home">
+      <h1 className="Home__title">Développeur Web</h1>
+      <p className="Home__text">
+        Je suis Mathéo Stunault, un développeur web passionné. Je crois en la
+        puissance du code pour créer des expériences en ligne exceptionnelles et
+        fonctionnelles. Mon objectif est de donner vie aux idées et de
+        transformer les concepts en réalité numérique. J'ai acquis de
+        l'expérience dans le développement front-end et back-end, en travaillant
+        avec des langages tels que HTML, CSS, JavaScript, ainsi que des
+        frameworks populaires. Mon approche est axée sur la recherche de
+        solutions techniques et sur la création d'interfaces utilisateur
+        intuitives. Je vous invite à découvrir mes projets les plus récents et
+        réussis. Chaque projet représente un défi que j'ai relevé avec le plus
+        grand intérêt. Je crois en la collaboration étroite avec mes clients, en
+        écoutant attentivement leurs besoins et en fournissant des solutions sur
+        mesure. Je suis toujours à l'affût des dernières tendances du
+        développement web et je cherche constamment à me perfectionner. Ma
+        curiosité et ma volonté d'apprendre de nouvelles technologies sont mes
+        moteurs pour créer des produits de haute qualité. Je vous invite à
+        parcourir mon portfolio et à découvrir mon travail. Merci d'avoir visité
+        mon portfolio et j'espère avoir l'occasion de collaborer avec vous
+        bientôt !
+      </p>
+    </div>
+  )
+}
+
+export default Home
